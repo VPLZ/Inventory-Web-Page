49dd1d198ac4c2a2d703e4d18aa602cd4e105887
Plazma <vdzlucas@gmail.com>
2025-01-22 14:16:56 +0100

    f

---
+++ public/script.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9MtjA_03b3ki7gl28pP5aO_yoyS0HWBE",
+  authDomain: "stock-banana.firebaseapp.com",
+  databaseURL: "https://stock-banana-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "stock-banana",
+  storageBucket: "stock-banana.firebasestorage.app",
+  messagingSenderId: "750264543978",
+  appId: "1:750264543978:web:d076b501593b31bf454593"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
